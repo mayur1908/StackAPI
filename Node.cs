2c9d1e463de51c94aeda72ca9a0073e5563f3e49
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-23 13:11:23 +0530

    [added simple linkedlist]

---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Data;    // Data stored in the node
+        public Node Next;   // Reference to the next node
+
+        public Node(int data)
+        {
+            Data = data;    // Assign the data value to the node
+            Next = null;    // Set the next node reference to null initially
+        }
+    }
+}
